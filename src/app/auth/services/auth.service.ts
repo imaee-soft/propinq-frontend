7c4e1171b2999f00a97fdfa18a408203b0c7f559
Ezequiel Sosa <ezequielsosa423@gmail.com>
2025-06-23 23:43:10 -0300

    feat: (WIP) new building creation feature with dialog and routing updates

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { AuthStatus } from '../enums/auth-status.enum';
+import { Role } from '../enums/role.enum';
 import { AuthState } from '../interfaces/auth-state.interface';
 
 const INITIAL_STATE: AuthState = {
-  user: null,
-  status: AuthStatus.PENDING,
+  user: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: '',
+    role: Role.OWNER,
+    phoneNumber: '',
+  },
+  status: AuthStatus.AUTHENTICATED,
 };
 
 @Injectable({ providedIn: 'root' })

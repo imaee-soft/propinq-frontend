623b2f22bcd8a0d79116df72441d689bb82da31a
Agustin Sorti <agustinsorti@gmail.com>
2025-08-24 01:57:15 -0300

    Active attribute

---
+++ src/app/auth/interfaces/realEstateResponse.interface.ts
@@ -0,0 +1,15 @@
+export interface RealEstateResponse {
+  id: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  companyName: string;
+  cuit: string;
+  legalName: string;
+  active: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}

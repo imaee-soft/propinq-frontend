f0a2de20460c928fde55e1210e69a9a08f916b1f
Ignacio Gonzalez <ignacio.rover@gmail.com>
2025-10-28 19:51:48 -0300

    import HttpParams on properties.service.ts

--- src/app/properties/properties.service.ts
+++ src/app/properties/properties.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable, throwError } from 'rxjs';
 import { environment } from '../../environments/environment.development';

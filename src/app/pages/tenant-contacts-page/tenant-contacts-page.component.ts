a5da5cd3660c2c0fa91f4ddb07ec4c882b0866f2
Ezequiel Sosa <ezequielsosa423@gmail.com>
2026-01-13 23:38:32 -0300

    feat: update UI components and improve layout; adjust pagination and data handling

--- src/app/pages/tenant-contacts-page/tenant-contacts-page.component.ts
+++ src/app/pages/tenant-contacts-page/tenant-contacts-page.component.ts
@@ -1,47 +1,26 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { of } from 'rxjs';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { tap } from 'rxjs';
 import { ContactsService } from '../../contacts/contacts.service';
 import { ContactDetails } from '../../contacts/interfaces/contact-details.interface';
 import { DEFAULT_CENTER } from '../../maps/utils/constants';
 import { CardDescriptor } from '../../shared/interfaces/card-descriptor.interface';
-import { Page } from '../../shared/interfaces/page.interface';
 import { CommonEntityPageComponent } from '../../shared/pages/common-entity-page/common-entity-page.component';
+import { NotificationService } from '../../shared/services/notification.service';
 
 @Component({
   selector: 'app-tenant-contacts-page',
   imports: [CommonEntityPageComponent],
   templateUrl: './tenant-contacts-page.component.html',
-  styleUrl: './tenant-contacts-page.component.css',
+  styleUrls: ['./tenant-contacts-page.component.css'],
 })
-export class TenantContactsPageComponent {
+export class TenantContactsPageComponent implements OnInit {
   private _contactsService = inject(ContactsService);
+  private _notificationService = inject(NotificationService);
 
-  hasToQuery = signal<boolean>(true);
+  canQuery = signal<boolean>(true);
   pageIndex = signal(0);
-
-  contactsDetailsResource = rxResource({
-    loader: () => {
-      if (this.hasToQuery()) {
-        return this._contactsService.getContactsDetails(this.pageIndex());
-      }
-      return of(null);
-    },
-  });
-
-  readonly page = computed<Page<ContactDetails> | null>(() => {
-    const value = this.contactsDetailsResource.value();
-    if (!value) return null;
-
-    return {
-      content: value.content,
-      total: value.totalElements,
-    };
-  });
-
-  totalElements = computed<number>(
-    () => this.contactsDetailsResource.value()?.totalElements || 0
-  );
+  contacts = signal<ContactDetails[]>([]);
+  totalElements = signal(0);
 
   descriptor: CardDescriptor<ContactDetails> = {
     user: (p) => p.owner ?? '',
@@ -56,18 +35,53 @@ export class TenantContactsPageComponent {
     secondaryActionLabel: (p) => 'Eliminar',
   };
 
+  ngOnInit(): void {
+    this.loadContacts();
+  }
+
+  loadContacts() {
+    if (!this.canQuery()) return;
+    this._contactsService
+      .getContactsDetails(this.pageIndex())
+      .pipe(
+        tap((newContacts) => {
+          this.contacts.set([...this.contacts(), ...newContacts.content]);
+          this.totalElements.set(newContacts.totalElements);
+          if (newContacts.totalElements === this.contacts().length)
+            this.canQuery.set(false);
+        })
+      )
+      .subscribe();
+  }
+
   loadMore = () => {
     this.pageIndex.update((i) => i + 1);
-    this.contactsDetailsResource.reload();
+    this.loadContacts();
   };
 
   delete = (contactId: string | number | undefined) => {
-    const contact = this.contactsDetailsResource
-      .value()
-      ?.content.find((c) => c.contactId === contactId);
+    const contact = this.contacts().find((c) => c.contactId === contactId);
     if (!contact) return;
-    this._contactsService.deleteContact(contact.contactId).subscribe(() => {
-      this.contactsDetailsResource.reload();
+    this._contactsService.deleteContact(contact.contactId).subscribe({
+      next: () => {
+        this._notificationService.success(
+          'La solicitud de contacto fue eliminada correctamente'
+        );
+        this.resetPage();
+        this.loadContacts();
+      },
+      error: () => {
+        this._notificationService.error(
+          'Ocurrió un error al eliminar el contacto'
+        );
+      },
     });
   };
+
+  private resetPage() {
+    this.contacts.set([]);
+    this.totalElements.set(0);
+    this.pageIndex.set(0);
+    this.canQuery.set(true);
+  }
 }

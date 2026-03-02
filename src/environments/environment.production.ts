025f3f1ca8e95de4c3083d052ad82f809ea1cc37
Your Name <ubuntu@ip-172-31-8-163.sa-east-1.compute.internal>
2026-03-02 17:18:05 +0000

    feat: add production configuration and Docker setup for deployment
    - Updated angular.json to include file replacements for production environment.
    - Added Dockerfile.prod for building and serving the Angular application with Nginx.
    - Created nginx.conf for proper routing of Angular SPA.
    - Introduced environment.production.ts for production-specific settings.

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  addressesUrl: 'https://nominatim.openstreetmap.org',
+  markerIconUrl: 'https://openlayers.org/en/latest/examples/data/icon.png',
+  apiUrl: '',
+  reCAPTCHA_SiteKey: '6LdbPUQsAAAAAClNS4JkyqHq1Jl2I2sszAEpGb0y',
+};
+
